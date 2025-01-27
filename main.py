15408dde6e30bb300e1e77d23d1ea2233419beeb
VictorAJM <victorajm@outlook.com>
2025-01-27 14:39:40 -0600

    reading data from json file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import argparse
 from helper import *
+import json
+
+
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--handle', type=str, help="Codeforces handle", required=True)
-  parser.add_argument('--problems', type=int, help="Number of problems", required=True)
-  parser.add_argument('--min', type=int, help="Minimum difficulty", required=True)
-  parser.add_argument('--max', type=int, help="Maximum difficulty", required=True)
-  args = parser.parse_args()
-
-  print(args.handle, args.problems, args.min, args.max)
+  # Open and read the JSON file
+  with open('data.json', 'r') as file:
+    data = json.load(file)
+    print(data['handle'], data['problems'], data['difficulty'])
 
 
 if __name__ == "__main__":
