d43cf895b62a0a63c000f299e0912c49b92dfd7c
VictorAJM <victorajm@outlook.com>
2025-01-27 14:35:58 -0600

    adding parameter to the problems creation

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from helper import *
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--handle', type=str, help="Codeforces handle", required=True)
+  parser.add_argument('--problems', type=int, help="Number of problems", required=True)
+  parser.add_argument('--min', type=int, help="Minimum difficulty", required=True)
+  parser.add_argument('--max', type=int, help="Maximum difficulty", required=True)
   args = parser.parse_args()
 
-  print(averageRatingSolvedProblems(args.handle))
+  print(args.handle, args.problems, args.min, args.max)
 
 
 if __name__ == "__main__":
